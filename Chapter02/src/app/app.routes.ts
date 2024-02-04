503d00762f0d6a10287e3e0b5c3d6cfa70b7e92b
Ricardo <ricardo.rv@outlook.com>
2024-02-04 20:37:25 +0100

    feat: add article component and redirections

--- Chapter02/src/app/app.routes.ts
+++ Chapter02/src/app/app.routes.ts
@@ -2,5 +2,12 @@ import { Routes } from '@angular/router';
 import { ContactComponent } from './features/contact/contact.component';
 
 export const routes: Routes = [
-  { path: 'contact', component: ContactComponent }
+  { path: 'contact', component: ContactComponent },
+  {
+    path: 'articles',
+    loadChildren: () =>
+      import('./features/articles/routes').then((mod) => mod.ARTICLES_ROUTES),
+  },
+  { path: '', pathMatch: 'full', redirectTo: 'articles' },
+  { path: '**', redirectTo: 'articles' },
 ];
---
+++ Chapter02/src/app/features/articles/routes.ts
@@ -0,0 +1,6 @@
+import { Route } from "@angular/router";
+import { ArticlesComponent } from "./articles.component";
+
+export const ARTICLES_ROUTES: Route[] = [
+  { path: '', component: ArticlesComponent }
+]
