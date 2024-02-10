3aef53a9ece5a6b195c1ef32e8b19e6155bf39ac
Ricardo <ricardo.rv@outlook.com>
2024-02-10 12:12:44 +0100

    feat: add issue report component

---
+++ Chapter03/src/app/issues/issue-report/issue-report.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-report',
+  standalone: true,
+  imports: [],
+  templateUrl: './issue-report.component.html',
+  styleUrl: './issue-report.component.css'
+})
+export class IssueReportComponent {
+
+}
