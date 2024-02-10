c867f6b6098efe8ce0c8fc19d4c6073a77e3611f
Ricardo <ricardo.rv@outlook.com>
2024-02-10 12:10:57 +0100

    feat: add clarity table

--- Chapter03/src/app/app.config.ts
+++ Chapter03/src/app/app.config.ts
@@ -1,13 +1,11 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
-import { ClarityModule } from '@clr/angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom([ClarityModule]),
     provideAnimations()
   ],
 };
--- Chapter03/src/app/issues/issue-list/issue-list.component.ts
+++ Chapter03/src/app/issues/issue-list/issue-list.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { IssuesService } from '../issues.service';
+import { Issue } from '../issue';
+import { ClarityModule } from '@clr/angular';
 
 @Component({
   selector: 'app-issue-list',
   standalone: true,
-  imports: [],
+  imports: [ClarityModule],
   templateUrl: './issue-list.component.html',
-  styleUrl: './issue-list.component.css'
+  styleUrl: './issue-list.component.css',
 })
 export class IssueListComponent {
-
+  private issueService = inject(IssuesService);
+  issues = signal<Issue[]>(this.issueService.getPendingIssues());
 }
