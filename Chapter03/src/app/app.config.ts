a6ce310669c6a9c51fa453370940f623e7c7c181
Ricardo <ricardo.rv@outlook.com>
2024-02-10 11:52:26 +0100

    chore: add clarityModule

--- Chapter03/src/app/app.config.ts
+++ Chapter03/src/app/app.config.ts
@@ -1,8 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { ClarityModule } from '@clr/angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([ClarityModule]),
+    provideAnimations()
+  ],
 };
