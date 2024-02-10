26b18610c6e2bca773fcf398ee239257215f0ff0
Ricardo <ricardo.rv@outlook.com>
2024-02-10 12:00:09 +0100

    feat: add issue list component

--- Chapter03/src/app/app.component.ts
+++ Chapter03/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { IssueListComponent } from './issues/issue-list/issue-list.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet, IssueListComponent]
 })
 export class AppComponent {
   title = 'issue-tracker';
---
+++ Chapter03/src/app/issues/issue-list/issue-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './issue-list.component.html',
+  styleUrl: './issue-list.component.css'
+})
+export class IssueListComponent {
+
+}
