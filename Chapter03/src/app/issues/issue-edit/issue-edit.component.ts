807ca09a731fc16f5b8c7e434e7b77d7fefe52e1
Ricardo <ricardo.rv@outlook.com>
2024-02-11 18:56:58 +0100

    feat: call to update issue in service when save issue method is triggered

--- Chapter03/src/app/issues/issue-edit/issue-edit.component.ts
+++ Chapter03/src/app/issues/issue-edit/issue-edit.component.ts
@@ -1,8 +1,9 @@
-import { Component, input, effect } from '@angular/core';
+import { Component, input, effect, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ClarityModule } from '@clr/angular';
 import { IssueForm } from '../issue-form';
 import { Issue } from '../issue';
+import { IssuesService } from '../issues.service';
 
 @Component({
   selector: 'app-issue-edit',
@@ -12,14 +13,22 @@ import { Issue } from '../issue';
   styleUrl: './issue-edit.component.css',
 })
 export class IssueEditComponent {
-  saveIssue() {
-    throw new Error('Method not implemented.');
-  }
+  private issueService = inject(IssuesService);
+
   issue = input.required<Issue>();
   issueForm!: FormGroup<IssueForm>;
 
   constructor() {
     effect(() => this.buildForm(this.issue()));
+  }
+
+  saveIssue(): void {
+    const issueToSave: Issue = {
+      ...this.issueForm.getRawValue() as Issue,
+      issueNo: this.issue().issueNo
+    };
+
+    this.issueService.updateIssue(issueToSave);
   }
 
   private buildForm(issue: Issue): void {
